61153d55a419226a3ad469a05cb9310e849e2f58
Roman Lebedev <lebedev.ri@gmail.com>
2025-06-20 19:10:39 +0300

    Implement `BitVacuumerJPEG`

--- src/memory/bitstream/mod.rs
+++ src/memory/bitstream/mod.rs
@@ -4,6 +4,7 @@ pub mod bitstream {
     use super::bitstreamflow::Bitwidth;
     use crate::bitstreamflow::BitStreamCache;
     use core::default::Default;
+    use core::result::Result::Ok;
     use core::unreachable;
     use rawspeed_memory_endianness::endianness::Endianness;
     use rawspeed_memory_endianness::endianness::SwapBytes;
@@ -16,6 +17,7 @@ pub mod bitstream {
         MSB,
         MSB16,
         MSB32,
+        JPEG,
     }
 
     pub trait BitOrderTrait {}
@@ -119,6 +121,28 @@ pub mod bitstream {
 
     //--------------------------------------------------------------------------
 
+    pub struct BitOrderJPEG;
+
+    impl BitOrderTrait for BitOrderJPEG {}
+
+    impl BitStreamTraits for BitOrderJPEG {
+        const TAG: BitOrder = BitOrder::JPEG;
+
+        type StreamFlow = super::bitstreamflow::BitStreamCacheLowInHighOut;
+
+        const FIXED_SIZE_CHUNKS: bool = false; // Stuffing byte...
+
+        type ChunkType = u32;
+
+        const CHUNK_ENDIANNESS: Endianness = Endianness::Big;
+
+        const MIN_LOAD_STEP_BYTE_MULTIPLE: u32 = 1; // FIXME
+    }
+
+    pub type BitVacuumerJPEG<'a, W> = BitVacuumerBase<'a, BitOrderJPEG, W>;
+
+    //--------------------------------------------------------------------------
+
     pub trait BitVacuumerTraits {
         const NEEDS_CUSTOM_DRAIN: bool = false; // Eww...
     }
@@ -127,6 +151,10 @@ pub mod bitstream {
     impl BitVacuumerTraits for BitOrderMSB {}
     impl BitVacuumerTraits for BitOrderMSB16 {}
     impl BitVacuumerTraits for BitOrderMSB32 {}
+
+    impl BitVacuumerTraits for BitOrderJPEG {
+        const NEEDS_CUSTOM_DRAIN: bool = true; // Eww...
+    }
 
     pub trait BitVacuumerDrainImpl {
         fn drain_impl(&mut self) -> std::io::Result<()>;
@@ -167,6 +195,65 @@ pub mod bitstream {
     impl_default_BitVacuumerCustomDrainImpl!(BitOrderMSB16);
     impl_default_BitVacuumerCustomDrainImpl!(BitOrderMSB32);
 
+    impl<'a, W> BitVacuumerCustomDrainImpl for BitVacuumerBase<'a, BitOrderJPEG, W>
+    where
+        BitOrderJPEG: BitOrderTrait + BitStreamTraits + BitVacuumerTraits,
+        W: std::io::Write,
+        u32: From<u8>
+            + Bitwidth
+            + From<<BitOrderJPEG as BitStreamTraits>::ChunkType>
+            + std::ops::ShlAssign<usize>
+            + std::ops::BitOrAssign,
+        <BitOrderJPEG as BitStreamTraits>::StreamFlow:
+            super::bitstreamflow::BitStreamCache,
+        <BitOrderJPEG as BitStreamTraits>::ChunkType: Bitwidth
+            + SwapBytes<<BitOrderJPEG as BitStreamTraits>::ChunkType>
+            + TryFrom<u64>,
+    {
+        fn custom_drain_impl(&mut self) -> std::io::Result<()> {
+            #[allow(clippy::assertions_on_constants)]
+            const {
+                assert!(BitOrderJPEG::NEEDS_CUSTOM_DRAIN);
+            }
+
+            assert!(self.cache.fill_level() >= u32::BITWIDTH);
+
+            let stream_chunk_bitwidth: usize =
+                <BitOrderJPEG as BitStreamTraits>::ChunkType::BITWIDTH;
+
+            assert!(u32::BITWIDTH == stream_chunk_bitwidth);
+
+            let chunk =
+                match <<BitOrderJPEG as BitStreamTraits>::ChunkType>::try_from(
+                    self.cache.peek(stream_chunk_bitwidth),
+                ) {
+                    Ok(t) => t,
+                    Err(_) => panic!("lossless cast failed?"),
+                };
+            self.cache.skip(stream_chunk_bitwidth);
+            let chunk: <BitOrderJPEG as BitStreamTraits>::ChunkType =
+                get_byte_swapped(
+                    chunk,
+                    <BitOrderJPEG as BitStreamTraits>::CHUNK_ENDIANNESS
+                        != Endianness::Little,
+                );
+            let bytes = chunk.to_le_bytes();
+
+            if bytes.iter().all(|byte| *byte != 0xFFu8) {
+                return self.writer.write_all(&bytes);
+            }
+
+            for byte in bytes {
+                self.writer.write_all(&[byte])?;
+                if byte == 0xFFu8 {
+                    const STUFFING_BYTE: u8 = 0x00u8;
+                    self.writer.write_all(&[STUFFING_BYTE])?;
+                }
+            }
+            Ok(())
+        }
+    }
+
     pub struct BitVacuumerBase<'a, T, W>
     where
         T: BitOrderTrait + BitStreamTraits + BitVacuumerTraits,
@@ -197,9 +284,7 @@ pub mod bitstream {
         T::ChunkType: Bitwidth + SwapBytes<T::ChunkType> + TryFrom<u64>,
     {
         fn drain_impl(&mut self) -> std::io::Result<()> {
-            const {
-                assert!(!T::NEEDS_CUSTOM_DRAIN);
-            }
+            assert!(!T::NEEDS_CUSTOM_DRAIN);
 
             assert!(self.cache.fill_level() >= u32::BITWIDTH);
 
@@ -1115,6 +1200,283 @@ mod tests_msb32 {
             vec![252, 3, 0, 0],
             vec![254, 1, 0, 0],
             vec![255, 0, 0, 0],
+        ];
+        assert_eq!(res, expected);
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests_jpeg {
+    use core::result::Result::Ok;
+    use std::io::Write;
+
+    use super::bitstream::*;
+
+    #[test]
+    fn vec_ctor_test() {
+        use std::io::Cursor;
+        let mut buf: Cursor<_> = Cursor::new(vec![]);
+        let _vac = BitVacuumerJPEG::new(&mut buf);
+    }
+
+    #[test]
+    fn arr_ctor_test() {
+        use std::io::Cursor;
+        let mut buf = [0u8; 1024];
+        let mut buf = Cursor::new(buf.as_mut());
+        let _vac = BitVacuumerJPEG::new(&mut buf);
+    }
+
+    #[test]
+    fn drop_empty_test() -> std::io::Result<()> {
+        use std::io::Cursor;
+        let mut buf = Cursor::new(vec![]);
+        let vac = BitVacuumerJPEG::new(&mut buf);
+        drop(vac);
+        buf.flush()?;
+        assert!(&buf.get_ref().is_empty());
+        Ok(())
+    }
+
+    #[test]
+    fn flush_empty_test() -> std::io::Result<()> {
+        use std::io::Cursor;
+        let mut buf = Cursor::new(vec![]);
+        let vac = BitVacuumerJPEG::new(&mut buf);
+        vac.flush()?;
+        buf.flush()?;
+        assert!(&buf.get_ref().is_empty());
+        Ok(())
+    }
+
+    #[test]
+    #[should_panic(
+        expected = "Unrecoverable Error: trying to drop non-empty BitVacuumer. Did you forget to call `flush()`?"
+    )]
+    fn dropping_unflushed_vac_byte() {
+        use std::io::Cursor;
+        let mut buf = Cursor::new(vec![]);
+        let mut vac = BitVacuumerJPEG::new(&mut buf);
+        match vac.put(0, 1) {
+            Ok(_) => (),
+            Err(_) => panic!("unexpected panic"),
+        }
+        drop(vac);
+    }
+
+    #[test]
+    fn flush_arr_overflow_test() -> std::io::Result<()> {
+        use std::io::Cursor;
+        let mut buf = [0u8; 0];
+        let mut buf = Cursor::new(buf.as_mut());
+        let mut vac = BitVacuumerJPEG::new(&mut buf);
+        vac.put(0, 1)?;
+        assert!(vac.flush().is_err());
+        Ok(())
+    }
+
+    #[test]
+    fn byte_enumeration_test() -> std::io::Result<()> {
+        let mut res: Vec<Vec<u8>> = vec![];
+        for num_bytes in 0..(8 + 1) {
+            use std::io::Cursor;
+            let mut buf = Cursor::new(vec![]);
+            let mut vac = BitVacuumerJPEG::new(&mut buf);
+            for i in 0..num_bytes {
+                vac.put(1 + i, 8)?;
+            }
+            vac.flush()?;
+            buf.flush()?;
+            res.push(buf.get_ref().clone());
+        }
+        let expected: Vec<Vec<u8>> = vec![
+            vec![],
+            vec![1, 0, 0, 0],
+            vec![1, 2, 0, 0],
+            vec![1, 2, 3, 0],
+            vec![1, 2, 3, 4],
+            vec![1, 2, 3, 4, 5, 0, 0, 0],
+            vec![1, 2, 3, 4, 5, 6, 0, 0],
+            vec![1, 2, 3, 4, 5, 6, 7, 0],
+            vec![1, 2, 3, 4, 5, 6, 7, 8],
+        ];
+        assert_eq!(res, expected);
+        Ok(())
+    }
+
+    #[test]
+    fn nibble_enumeration_test() -> std::io::Result<()> {
+        let mut res: Vec<Vec<u8>> = vec![];
+        for num_nibbles in 0..16 {
+            use std::io::Cursor;
+            let mut buf = Cursor::new(vec![]);
+            let mut vac = BitVacuumerJPEG::new(&mut buf);
+            for i in 0..num_nibbles {
+                let nibble = 1 + i;
+                assert!(nibble <= 0xF);
+                vac.put(nibble, 4)?;
+            }
+            vac.flush()?;
+            buf.flush()?;
+            res.push(buf.get_ref().clone());
+        }
+        let expected: Vec<Vec<u8>> = vec![
+            vec![],
+            vec![16, 0, 0, 0],
+            vec![18, 0, 0, 0],
+            vec![18, 48, 0, 0],
+            vec![18, 52, 0, 0],
+            vec![18, 52, 80, 0],
+            vec![18, 52, 86, 0],
+            vec![18, 52, 86, 112],
+            vec![18, 52, 86, 120],
+            vec![18, 52, 86, 120, 144, 0, 0, 0],
+            vec![18, 52, 86, 120, 154, 0, 0, 0],
+            vec![18, 52, 86, 120, 154, 176, 0, 0],
+            vec![18, 52, 86, 120, 154, 188, 0, 0],
+            vec![18, 52, 86, 120, 154, 188, 208, 0],
+            vec![18, 52, 86, 120, 154, 188, 222, 0],
+            vec![18, 52, 86, 120, 154, 188, 222, 240],
+        ];
+        assert_eq!(res, expected);
+        Ok(())
+    }
+
+    #[test]
+    fn bit_enumeration_test() -> std::io::Result<()> {
+        let mut res: Vec<Vec<u8>> = vec![];
+        for num_leading_zeros in 0..32 {
+            use std::io::Cursor;
+            let mut buf = Cursor::new(vec![]);
+            let mut vac = BitVacuumerJPEG::new(&mut buf);
+            for _i in 0..num_leading_zeros {
+                vac.put(0, 1)?;
+            }
+            vac.put(1, 1)?;
+            vac.flush()?;
+            buf.flush()?;
+            res.push(buf.get_ref().clone());
+        }
+        let expected: Vec<Vec<u8>> = vec![
+            vec![128, 0, 0, 0],
+            vec![64, 0, 0, 0],
+            vec![32, 0, 0, 0],
+            vec![16, 0, 0, 0],
+            vec![8, 0, 0, 0],
+            vec![4, 0, 0, 0],
+            vec![2, 0, 0, 0],
+            vec![1, 0, 0, 0],
+            vec![0, 128, 0, 0],
+            vec![0, 64, 0, 0],
+            vec![0, 32, 0, 0],
+            vec![0, 16, 0, 0],
+            vec![0, 8, 0, 0],
+            vec![0, 4, 0, 0],
+            vec![0, 2, 0, 0],
+            vec![0, 1, 0, 0],
+            vec![0, 0, 128, 0],
+            vec![0, 0, 64, 0],
+            vec![0, 0, 32, 0],
+            vec![0, 0, 16, 0],
+            vec![0, 0, 8, 0],
+            vec![0, 0, 4, 0],
+            vec![0, 0, 2, 0],
+            vec![0, 0, 1, 0],
+            vec![0, 0, 0, 128],
+            vec![0, 0, 0, 64],
+            vec![0, 0, 0, 32],
+            vec![0, 0, 0, 16],
+            vec![0, 0, 0, 8],
+            vec![0, 0, 0, 4],
+            vec![0, 0, 0, 2],
+            vec![0, 0, 0, 1],
+        ];
+        assert_eq!(res, expected);
+        Ok(())
+    }
+
+    #[test]
+    fn sliding_0xff_test() -> std::io::Result<()> {
+        let mut res: Vec<Vec<u8>> = vec![];
+        for num_leading_zeros in 0..(32 - 8 + 1) {
+            use std::io::Cursor;
+            let mut buf = Cursor::new(vec![]);
+            let mut vac = BitVacuumerJPEG::new(&mut buf);
+            for _i in 0..num_leading_zeros {
+                vac.put(0, 1)?;
+            }
+            vac.put(0xFF, 8)?;
+            vac.flush()?;
+            buf.flush()?;
+            res.push(buf.get_ref().clone());
+        }
+        let expected: Vec<Vec<u8>> = vec![
+            vec![255, 0, 0, 0, 0],
+            vec![127, 128, 0, 0],
+            vec![63, 192, 0, 0],
+            vec![31, 224, 0, 0],
+            vec![15, 240, 0, 0],
+            vec![7, 248, 0, 0],
+            vec![3, 252, 0, 0],
+            vec![1, 254, 0, 0],
+            vec![0, 255, 0, 0, 0],
+            vec![0, 127, 128, 0],
+            vec![0, 63, 192, 0],
+            vec![0, 31, 224, 0],
+            vec![0, 15, 240, 0],
+            vec![0, 7, 248, 0],
+            vec![0, 3, 252, 0],
+            vec![0, 1, 254, 0],
+            vec![0, 0, 255, 0, 0],
+            vec![0, 0, 127, 128],
+            vec![0, 0, 63, 192],
+            vec![0, 0, 31, 224],
+            vec![0, 0, 15, 240],
+            vec![0, 0, 7, 248],
+            vec![0, 0, 3, 252],
+            vec![0, 0, 1, 254],
+            vec![0, 0, 0, 255, 0],
+        ];
+        assert_eq!(res, expected);
+        Ok(())
+    }
+
+    #[test]
+    fn sliding_0xff_prefixed_by_enumerated_nibbles_test() -> std::io::Result<()>
+    {
+        let mut res: Vec<Vec<u8>> = vec![];
+        for num_leading_nibbles in 0..16 {
+            use std::io::Cursor;
+            let mut buf = Cursor::new(vec![]);
+            let mut vac = BitVacuumerJPEG::new(&mut buf);
+            for i in 0..num_leading_nibbles {
+                let nibble = 1 + i;
+                assert!(nibble <= 0xF);
+                vac.put(nibble, 4)?;
+            }
+            vac.put(0xFF, 8)?;
+            vac.flush()?;
+            buf.flush()?;
+            res.push(buf.get_ref().clone());
+        }
+        let expected: Vec<Vec<u8>> = vec![
+            vec![255, 0, 0, 0, 0],
+            vec![31, 240, 0, 0],
+            vec![18, 255, 0, 0, 0],
+            vec![18, 63, 240, 0],
+            vec![18, 52, 255, 0, 0],
+            vec![18, 52, 95, 240],
+            vec![18, 52, 86, 255, 0],
+            vec![18, 52, 86, 127, 240, 0, 0, 0],
+            vec![18, 52, 86, 120, 255, 0, 0, 0, 0],
+            vec![18, 52, 86, 120, 159, 240, 0, 0],
+            vec![18, 52, 86, 120, 154, 255, 0, 0, 0],
+            vec![18, 52, 86, 120, 154, 191, 240, 0],
+            vec![18, 52, 86, 120, 154, 188, 255, 0, 0],
+            vec![18, 52, 86, 120, 154, 188, 223, 240],
+            vec![18, 52, 86, 120, 154, 188, 222, 255, 0],
+            vec![18, 52, 86, 120, 154, 188, 222, 255, 0, 240, 0, 0, 0],
         ];
         assert_eq!(res, expected);
         Ok(())
