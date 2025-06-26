f6cd1d6476a797abc56a5ef4f3fd96d8e654b62c
Roman Lebedev <lebedev.ri@gmail.com>
2025-06-26 05:39:38 +0300

    BitVacuumerMSB32: add a few more tests

--- src/memory/bitvacuumer/bitvacuumer/msb32/test.rs
+++ src/memory/bitvacuumer/bitvacuumer/msb32/test.rs
@@ -92,6 +92,44 @@ fn byte_enumeration_test() -> std::io::Result<()> {
 }
 
 #[test]
+fn nibble_enumeration_test() -> std::io::Result<()> {
+    let mut res: Vec<Vec<u8>> = vec![];
+    for num_nibbles in 0..16 {
+        use std::io::Cursor;
+        let mut buf = Cursor::new(vec![]);
+        let mut vac = BitVacuumerMSB32::new(&mut buf);
+        for i in 0..num_nibbles {
+            let nibble = 1 + i;
+            assert!(nibble <= 0xF);
+            vac.put(nibble, 4)?;
+        }
+        vac.flush()?;
+        buf.flush()?;
+        res.push(buf.get_ref().clone());
+    }
+    let expected: Vec<Vec<u8>> = vec![
+        vec![],
+        vec![0, 0, 0, 16],
+        vec![0, 0, 0, 18],
+        vec![0, 0, 48, 18],
+        vec![0, 0, 52, 18],
+        vec![0, 80, 52, 18],
+        vec![0, 86, 52, 18],
+        vec![112, 86, 52, 18],
+        vec![120, 86, 52, 18],
+        vec![120, 86, 52, 18, 0, 0, 0, 144],
+        vec![120, 86, 52, 18, 0, 0, 0, 154],
+        vec![120, 86, 52, 18, 0, 0, 176, 154],
+        vec![120, 86, 52, 18, 0, 0, 188, 154],
+        vec![120, 86, 52, 18, 0, 208, 188, 154],
+        vec![120, 86, 52, 18, 0, 222, 188, 154],
+        vec![120, 86, 52, 18, 240, 222, 188, 154],
+    ];
+    assert_eq!(res, expected);
+    Ok(())
+}
+
+#[test]
 fn bit_enumeration_test() -> std::io::Result<()> {
     let mut res: Vec<Vec<u8>> = vec![];
     for num_leading_zeros in 0..32 {
@@ -185,6 +223,45 @@ fn sliding_0xff_test() -> std::io::Result<()> {
         vec![252, 3, 0, 0],
         vec![254, 1, 0, 0],
         vec![255, 0, 0, 0],
+    ];
+    assert_eq!(res, expected);
+    Ok(())
+}
+
+#[test]
+fn sliding_0xff_prefixed_by_enumerated_nibbles_test() -> std::io::Result<()> {
+    let mut res: Vec<Vec<u8>> = vec![];
+    for num_leading_nibbles in 0..16 {
+        use std::io::Cursor;
+        let mut buf = Cursor::new(vec![]);
+        let mut vac = BitVacuumerMSB32::new(&mut buf);
+        for i in 0..num_leading_nibbles {
+            let nibble = 1 + i;
+            assert!(nibble <= 0xF);
+            vac.put(nibble, 4)?;
+        }
+        vac.put(0xFF, 8)?;
+        vac.flush()?;
+        buf.flush()?;
+        res.push(buf.get_ref().clone());
+    }
+    let expected: Vec<Vec<u8>> = vec![
+        vec![0, 0, 0, 255],
+        vec![0, 0, 240, 31],
+        vec![0, 0, 255, 18],
+        vec![0, 240, 63, 18],
+        vec![0, 255, 52, 18],
+        vec![240, 95, 52, 18],
+        vec![255, 86, 52, 18],
+        vec![127, 86, 52, 18, 0, 0, 0, 240],
+        vec![120, 86, 52, 18, 0, 0, 0, 255],
+        vec![120, 86, 52, 18, 0, 0, 240, 159],
+        vec![120, 86, 52, 18, 0, 0, 255, 154],
+        vec![120, 86, 52, 18, 0, 240, 191, 154],
+        vec![120, 86, 52, 18, 0, 255, 188, 154],
+        vec![120, 86, 52, 18, 240, 223, 188, 154],
+        vec![120, 86, 52, 18, 255, 222, 188, 154],
+        vec![120, 86, 52, 18, 255, 222, 188, 154, 0, 0, 0, 240],
     ];
     assert_eq!(res, expected);
     Ok(())
