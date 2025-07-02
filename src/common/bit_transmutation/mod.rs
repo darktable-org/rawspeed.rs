b3c7613f1e5dfb758d8464a4af7ae1120d1f3d02
Roman Lebedev <lebedev.ri@gmail.com>
2025-07-02 04:41:14 +0300

    Extract CopyFromSlice/LoadFromSlice/FromNeBytes to common

--- src/common/bit_transmutation/mod.rs
+++ src/common/bit_transmutation/mod.rs
@@ -57,3 +57,60 @@ impl_to_ne_bytes!(u8);
 impl_to_ne_bytes!(u16);
 impl_to_ne_bytes!(u32);
 impl_to_ne_bytes!(u64);
+
+pub trait CopyFromSlice {
+    fn copy_from_slice_(&mut self, src: &[u8]);
+}
+
+impl CopyFromSlice for [u8] {
+    #[inline]
+    fn copy_from_slice_(&mut self, src: &[u8]) {
+        self.copy_from_slice(src);
+    }
+}
+
+pub trait LoadFromSlice<T>
+where
+    T: Default + core::ops::IndexMut<core::ops::RangeFull>,
+    <T as core::ops::Index<core::ops::RangeFull>>::Output: CopyFromSlice,
+{
+    fn load_from_slice(&self) -> T;
+}
+
+impl<T> LoadFromSlice<T> for [u8]
+where
+    T: Default + core::ops::IndexMut<core::ops::RangeFull>,
+    <T as core::ops::Index<core::ops::RangeFull>>::Output: CopyFromSlice,
+{
+    #[inline]
+    fn load_from_slice(&self) -> T {
+        let mut out: T = Default::default();
+        out[..].copy_from_slice_(self);
+        out
+    }
+}
+
+pub trait FromNeBytes {
+    type Output;
+
+    #[allow(clippy::wrong_self_convention)]
+    fn from_ne_bytes(self) -> Self::Output;
+}
+
+macro_rules! impl_from_ne_bytes {
+    ($tgt:ty) => {
+        impl FromNeBytes for [u8; core::mem::size_of::<$tgt>()] {
+            type Output = $tgt;
+
+            #[inline]
+            fn from_ne_bytes(self) -> Self::Output {
+                Self::Output::from_ne_bytes(self)
+            }
+        }
+    };
+}
+
+impl_from_ne_bytes!(u8);
+impl_from_ne_bytes!(u16);
+impl_from_ne_bytes!(u32);
+impl_from_ne_bytes!(u64);
