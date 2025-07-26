153e375f6b4042c9e7b4ba3ab917cd43ddab8ad8
Roman Lebedev <lebedev.ri@gmail.com>
2025-07-26 08:39:37 +0300

    `cameras.xml` xmllint

---
+++ tests/metadata/camerasxml_parser/xmllint.rs
@@ -0,0 +1,16 @@
+#[cfg(test)]
+mod tests {
+    #[test]
+    #[cfg(not(miri))]
+    fn xmllint() -> Result<(), Box<dyn core::error::Error>> {
+        use rawspeed_metadata_camerasxml_parser::camerasxml_parser;
+        let camerasxml_path =
+            rawspeed_utils_librstest::rstest::get_camerasxml_path();
+
+        let camerasxml_contents = std::fs::read_to_string(camerasxml_path)?;
+
+        let _repr = camerasxml_parser::parse_str(&camerasxml_contents)?;
+
+        Ok(())
+    }
+}
