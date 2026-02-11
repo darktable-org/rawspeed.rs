f0bcd1b1223536d411bdecc82ad4faa35a94ea88
Roman Lebedev <lebedev.ri@gmail.com>
2026-02-11 23:57:48 +0300

    Cargo: simplify workspace dependency inheritance

--- src/parsers/rawparser/rawparser/mod.rs
+++ src/parsers/rawparser/rawparser/mod.rs
@@ -1,4 +1,4 @@
-use rawspeed_demuxers_naked::naked_demuxer::NakedDemuxer;
+use rawspeed_demuxers_packed::naked_demuxer::NakedDemuxer;
 use rawspeed_demuxers_rawdemuxer::rawdemuxer::RawDemuxer;
 use rawspeed_memory_nd_slice_procurement::ndsliceprocurement::NDSliceProcurementRequest;
 use rawspeed_metadata_camerametadata::camerametadata::DecodeableCamera;
