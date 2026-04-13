4d9082e54ee03d07125135f6d3e742b6c8999760
Roman Lebedev <lebedev.ri@gmail.com>
2026-04-13 06:44:40 +0300

    Range rotation: dedup somewhat

--- src/std/range_rotation/range_rotation/rotated_range/mod.rs
+++ src/std/range_rotation/range_rotation/rotated_range/mod.rs
@@ -1,23 +1,11 @@
+use crate::range_rotation::rotated_range_iterator::RotatedRangeIterator;
+
 #[non_exhaustive]
 #[must_use]
 #[derive(Debug, Clone)]
 pub struct RotatedRange<Idx> {
     range: core::ops::Range<Idx>,
     mid: isize,
-}
-
-#[non_exhaustive]
-#[must_use]
-#[derive(Debug, Clone)]
-pub struct RotatedRangeIterator<Idx>
-where
-    core::ops::Range<Idx>: Iterator<Item = Idx>,
-{
-    bounds: core::ops::Range<Idx>,
-    range: core::iter::Chain<
-        <core::ops::Range<Idx> as IntoIterator>::IntoIter,
-        <core::ops::Range<Idx> as IntoIterator>::IntoIter,
-    >,
 }
 
 impl<Idx> RotatedRange<Idx> {
@@ -29,7 +17,7 @@ impl<Idx> RotatedRange<Idx> {
 
 impl<Idx> IntoIterator for RotatedRange<Idx>
 where
-    Idx: Copy,
+    Idx: Copy + PartialOrd,
     core::ops::Range<Idx>: Clone + Iterator<Item = Idx> + ExactSizeIterator,
     RotatedRangeIterator<Idx>: Iterator<Item = Idx>,
 {
@@ -39,49 +27,14 @@ where
 
     #[inline]
     fn into_iter(self) -> Self::IntoIter {
-        let mid_elt = match core::num::NonZero::try_from(self.range.len()) {
-            Ok(range_len) => {
-                let normalized_mid = match self.mid.try_into() {
-                    Ok(mid) => mid,
-                    Err(_) => {
-                        range_len.get() - (self.mid.unsigned_abs() % range_len)
-                    }
-                };
-                let normalized_mid = normalized_mid % range_len;
-                let elt = self.range.clone().nth(normalized_mid);
-                #[expect(unsafe_code, clippy::undocumented_unsafe_blocks)]
-                unsafe {
-                    elt.unwrap_unchecked()
+        RotatedRangeIterator::new(self.range, |range_len| -> usize {
+            match self.mid.try_into() {
+                Ok(mid) => mid,
+                Err(_) => {
+                    range_len.get() - (self.mid.unsigned_abs() % range_len)
                 }
             }
-            Err(_) => self.range.start,
-        };
-
-        let front_part = mid_elt..self.range.end;
-        let back_part = self.range.start..mid_elt;
-        RotatedRangeIterator {
-            bounds: self.range,
-            range: front_part.chain(back_part),
-        }
-    }
-}
-
-#[expect(clippy::missing_trait_methods)]
-impl<Idx> Iterator for RotatedRangeIterator<Idx>
-where
-    Idx: PartialOrd,
-    core::ops::Range<Idx>: Iterator<Item = Idx>,
-{
-    type Item = Idx;
-
-    #[inline]
-    fn next(&mut self) -> Option<Self::Item> {
-        let item = self.range.next()?;
-        #[expect(unsafe_code, clippy::undocumented_unsafe_blocks)]
-        unsafe {
-            core::hint::assert_unchecked(self.bounds.contains(&item));
-        }
-        Some(item)
+        })
     }
 }
 
